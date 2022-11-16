0f7c3f9e23c3e6ce79c97c41c5b9cbbe4a49519b
MMMike <mikem.tba@gmail.com>
2022-11-16 19:17:06 +0200

    feat: :wrench: Set build target to es2020

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
-import solid from "solid-start/vite";
-import { defineConfig } from "vite";
+import { readFileSync } from 'fs'
+import solid from 'solid-start/vite'
+import { defineConfig } from 'vite'
+
+const pkgJSON = JSON.parse(readFileSync('package.json', 'utf-8'))
 
 export default defineConfig({
   plugins: [solid()],
-});
+  build: {
+    target: 'es2020',
+  },
+})
