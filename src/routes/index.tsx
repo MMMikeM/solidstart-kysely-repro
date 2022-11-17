9a822abbf4753e423d9d8505d97640f1ec2e2e02
Mike Murray <mikem.tba@gmail.com>
2022-11-17 16:34:16 +0200

    feat: ✨ Display data from db

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
+import { For } from 'solid-js'
 import { Title, useRouteData } from 'solid-start'
 import { createServerData$ } from 'solid-start/server'
 import { testDb } from '~/server'
 
 export function routeData() {
   return createServerData$(async (_, { request }) => {
-    return await testDb()
+    const data = await testDb()
+    return data
   })
 }
 
@@ -15,7 +17,18 @@ export default function Home() {
     <main>
       <Title>Hello Kysely</Title>
       <h1>Hello world!</h1>
-      {JSON.stringify(data)}
+      <For each={[data()]}>
+        {({ id, test }) => <ShowData id={id} test={test} />}
+      </For>
     </main>
+  )
+}
+
+const ShowData = (props: { id: string; test: string }) => {
+  return (
+    <>
+      <h2>{props.id}</h2>
+      <h2>{props.test}</h2>
+    </>
   )
 }
