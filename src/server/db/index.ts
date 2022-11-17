c03fdd6320cb8aeb8e392d5c5803ce85491ee1ec
Mike Murray <mikem.tba@gmail.com>
2022-11-17 09:50:04 +0200

    fix: 🐛 Sort out export and migration call

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -3,7 +3,6 @@ import { Kysely, PostgresDialect } from 'kysely'
 import { Database } from './schema'
 import type { ConnectionConfig } from 'pg'
 import { migrateToLatest } from './migrate'
-import { readFileSync } from 'fs'
 
 const { Pool } = pg
 
@@ -15,11 +14,10 @@ const config: ConnectionConfig = {
   database: 'postgres',
 }
 
-const db = new Kysely<Database>({
+export const db = new Kysely<Database>({
   dialect: new PostgresDialect({
     pool: new Pool(config),
   }),
 })
 
 migrateToLatest(db)
-export { db }
--- src/server/db/migrate.ts
+++ src/server/db/migrate.ts
@@ -1,35 +1,33 @@
-import * as path from "path"
-import { promises as fs } from "fs"
-import { Migrator, FileMigrationProvider } from "kysely"
-import { db } from "."
+import * as path from 'path'
+import { promises as fs } from 'fs'
+import { Migrator, FileMigrationProvider } from 'kysely'
+import { db } from '.'
 
-async function migrateToLatest() {
+export async function migrateToLatest(db: any) {
   const migrator = new Migrator({
     db,
     provider: new FileMigrationProvider({
       fs,
       path,
-      migrationFolder: "./src/server/db/migrations/",
+      migrationFolder: './src/server/db/migrations/',
     }),
   })
 
   const { error, results } = await migrator.migrateToLatest()
 
   results?.forEach((it) => {
-    if (it.status === "Success") {
+    if (it.status === 'Success') {
       console.log(`migration "${it.migrationName}" was executed successfully`)
-    } else if (it.status === "Error") {
+    } else if (it.status === 'Error') {
       console.error(`failed to execute migration "${it.migrationName}"`)
     }
   })
 
   if (error) {
-    console.error("failed to migrate")
+    console.error('failed to migrate')
     console.error(error)
     process.exit(1)
   }
 
   await db.destroy()
 }
-
-migrateToLatest()
